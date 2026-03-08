7e4b839a15161f26ddd28f1c7ea91359e68780f7
Aryan Sharma <shaileshsharma3526@gmail.com>
2026-03-08 14:40:40 +0530

    update backend api url

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://gym-website-mern-1-zifl.onrender.com/send/mail",
         {
           name,
           email,
